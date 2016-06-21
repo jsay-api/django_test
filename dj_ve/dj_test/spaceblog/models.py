733bd5dafddf9d5c406c615961c3aff8a97b1f03
jsay-api <julia.sayapina@me.com>
2016-06-21 15:12:13 +0300

    spaceblog app added, model added, migrated

--- dj_ve/dj_test/spaceblog/models.py
+++ dj_ve/dj_test/spaceblog/models.py
@@ -1,5 +1,12 @@
+# coding: utf-8
+
 from __future__ import unicode_literals
 
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+	title = models.CharField('SpaceTitle', max_length = 100)
+	text = models.TextField('SpaceText')
+	create_date = models.DateTimeField('SpaceTime Sol#')
+	
