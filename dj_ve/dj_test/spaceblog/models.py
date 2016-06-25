023733a31f13b18452fa5e21ccea25211d3c8fd4
jsay-api <julia.sayapina@me.com>
2016-06-25 19:39:14 +0300

    pushed errything to git

--- dj_ve/dj_test/spaceblog/models.py
+++ dj_ve/dj_test/spaceblog/models.py
@@ -9,4 +9,6 @@ class Post(models.Model):
 	title = models.CharField('SpaceTitle', max_length = 100)
 	text = models.TextField('SpaceText')
 	create_date = models.DateTimeField('SpaceTime Sol#')
-	
+
+	def __unicode__(self):	#for Python 3.x this method is called __str__
+		return '{0} {1}'.format(self.title, self.create_date)	
